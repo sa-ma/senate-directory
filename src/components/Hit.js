398354c3ab29823761fa6aebfea23c7aededc077
Ladi Adenusi <ladisays@gmail.com>
2020-10-09 15:56:07 +0100

    update email body and package-lock.json

--- src/components/Hit.js
+++ src/components/Hit.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const message =
-  "Good day Senator/Speaker/Governor,%0D%0A %0D%0AI am a citizen and constituent of “insert area”. I’m writing you to bring to your attention the #ENDSARS protest currently underway in Nigeria.%0D%0AAs you know, SARS was created to help against armed robbers, however, SARS has spent more time harassing, stealing from and killing innocent people than they have apprehending criminals. The Nigerian government has tried to reform SARS at least 4 times, to no avail. For this reason, I am calling on you to protect your citizens and call for the disbanding and end of SARS. They can no longer be reformed. They should not be existing. Thank you.%0D%0A %0D%0ABest Regards";
+  'Good day Senator/Speaker/Governor,%0D%0A %0D%0AI am a citizen and constituent of “insert area”. I’m writing you to bring to your attention the #ENDSARS protest currently underway in Nigeria.%0D%0A %0D%0AAs you know, SARS was created to help against armed robbers, however, SARS has spent more time harassing, stealing from and killing innocent people than they have apprehending criminals. The Nigerian government has tried to reform SARS at least 4 times, to no avail.%0D%0A %0D%0AFor this reason, I am calling on you to protect your citizens and call for the disbanding and end of SARS. They can no longer be reformed. They should not be existing. Thank you.%0D%0A %0D%0ABest Regards';
 const Hit = ({ hit }) => (
   <div className="senator-details">
     <div className="senator-name">
